0efbda824262c14e73d01aea78f1c6b3114bf052
Dylan Perera <d.perera@mail.utoronto.ca>
2024-02-05 19:57:53 -0500

    Added setup files and created logo component for page

---
+++ uber-clone/src/components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class = "p-10">
+    <img class="w-40 " src = "https://cdn-assets-eu.frontify.com/s3/frontify-enterprise-files-eu/eyJwYXRoIjoid2VhcmVcL2ZpbGVcLzhGbTh4cU5SZGZUVjUxYVh3bnEyLnN2ZyJ9:weare:F1cOF9Bps96cMy7r9Y2d7affBYsDeiDoIHfqZrbcxAw?width=1200&height=417">
+  </div>
+</template>
+
+<script>
+
+</script>
+
---
+++ uber-clone/src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="background">
+  <Logo class = "logo"/>
+
+  <div class="main-info">
+    <div>
+      <p class="text-white"> Search here</p>
+      <!--Search component!-->
+    </div>
+    <div>
+      <img class="w-96" src = "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_1144,h_1430/v1684852612/assets/ba/4947c1-b862-400e-9f00-668f4926a4a2/original/Ride-with-Uber.png">
+    </div>
+  </div>
+
+
+</div>
+</template>
+
+<script>
+import Logo from '../components/Logo.vue'
+
+export default {
+  components: { Logo },
+  name: 'HomeView',
+}
+</script>
+
+<style scoped>
+
+
+
+.background{
+  height: 100vh;
+}
+
+.main-info{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100%;
+}
+
+.logo
+{
+  position: absolute;
+}
+
+</style>
